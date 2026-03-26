f12ad3fbf995da0079bf5973c3959584b206d709
Bella Gould <isabella.gould@utdallas.edu>
2026-03-26 12:10:15 -0500

    Added pseudocode, put in generate_level and generate_cake with pass functionality.

---
+++ project1.py
@@ -0,0 +1,15 @@
+# cake generator!
+# parameters: cake levels (integer), cake height (float), cake width (float),
+# cake color (???), icing (boolean), cake level proportion (float)
+# first I need to get Maya to generate a cylinder.
+# We'll call this function generate_level
+
+def generate_level():
+    pass
+
+
+# I'll need to call generate_level multiple times.
+# I can call this in another function, generate_cake.
+def generate_cake(levels, cake_height, cake_width, 
+                  level_proportion, cake_color, icing):
+    pass
