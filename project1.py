9bdd81c8abda87e7ba422330814926b696c4dfb5
Bella Gould <isabella.gould@utdallas.edu>
2026-03-26 12:24:31 -0500

    Roughly finished calculate_width.  I may return to put a better solution than the if/else statement.

--- project1.py
+++ project1.py
@@ -4,12 +4,29 @@
 # first I need to get Maya to generate a cylinder.
 # We'll call this function generate_level
 
-def generate_level():
+def generate_level(level_width, level_height):
     pass
 
+def generate_icing():
+    pass
+
+def calculate_width(level, cake_width, level_proportion):
+    # this is where we apply the level_proportion to the cake level.
+    # we want the levels to get progressively smaller as they increase.
+    # on level 0, we want the width to be unchanged.
+    # level_width = cake_width
+    if level == 0:
+        level_width = cake_width
+    else:
+        level_width = level*level_proportion*cake_width
+    return level_width
 
 # I'll need to call generate_level multiple times.
 # I can call this in another function, generate_cake.
-def generate_cake(levels, cake_height, cake_width, 
-                  level_proportion, cake_color, icing):
-    pass
+def generate_cake(levels=1, cake_height=3.0, cake_width=1.0, 
+                  level_proportion=0.75, cake_color="white", icing=False):
+    for level in range(0, levels):
+        level_width = calculate_width(level, cake_width, level_proportion)
+        generate_level(level_width, cake_height)
+        if icing == True:
+            generate_icing():
