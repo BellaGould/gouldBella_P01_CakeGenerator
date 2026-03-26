e3bf595a395ba0b4091ea7ed94b46b6d7d91ca7c
Bella Gould <isabella.gould@utdallas.edu>
2026-03-26 12:41:05 -0500

    Implemented rudimentary functionality for generate_level. It generates a cylinder but does not transform it or pivot point.

--- project1.py
+++ project1.py
@@ -1,3 +1,4 @@
+import maya.cmds as cmds
 # cake generator!
 # parameters: cake levels (integer), cake height (float), cake width (float),
 # cake color (???), icing (boolean), cake level proportion (float)
@@ -5,10 +6,12 @@
 # We'll call this function generate_level
 
 def generate_level(level_width, level_height):
-    pass
+    lvl_name = cmds.polyCylinder(height=level_height, radius=(level_width/2))[0]
+    return lvl_name
 
 def generate_icing():
     pass
+    return icing_name
 
 def calculate_width(level, cake_width, level_proportion):
     # this is where we apply the level_proportion to the cake level.
@@ -23,10 +26,14 @@ def calculate_width(level, cake_width, level_proportion):
 
 # I'll need to call generate_level multiple times.
 # I can call this in another function, generate_cake.
-def generate_cake(levels=1, cake_height=3.0, cake_width=1.0, 
+def generate_cake(levels=3, cake_height=3.0, cake_width=1.0, 
                   level_proportion=0.75, cake_color="white", icing=False):
+    cake_list= []
+    level_height = cake_height/levels
     for level in range(0, levels):
         level_width = calculate_width(level, cake_width, level_proportion)
-        generate_level(level_width, cake_height)
+        lvl_name = generate_level(level_width, level_height)
+        cake_list.append(lvl_name)
         if icing == True:
-            generate_icing():
+            icing_name = generate_icing(level_width, level_height):
+            cake_list.append(icing_name)
