fab29c0b17c745ffeaa5a3660d0f9aceb0222799
Bella Gould <isabella.gould@utdallas.edu>
2026-04-01 15:54:13 -0500

    Cleaned up code, added notes on how to operate Maya script editor.

--- project1.py
+++ project1.py
@@ -1,5 +1,5 @@
 import maya.cmds as cmds
-from PySide6 import QtWidgets, QtCore
+# from PySide6 import QtWidgets, QtCore
 # cake generator!
 # parameters: cake levels (integer), cake height (float), cake width (float),
 # cake color (???), icing (boolean), cake level proportion (float)
@@ -16,13 +16,13 @@ class Cake():
     cake_color = "white"
     icing = True
     
-    def generate_level(self, level_width, level_height):
-        level_lift = self.level*self.level_height
+    def generate_level(self, level, level_width, level_height):
+        level_lift = level*level_height
         lvl_name = cmds.polyCylinder(height=level_height,
                                      radius=level_width)[0]
-        cmds.xform(lvl_name, pivots=[0, -self.level_height/2, 0])
-        cmds.xform(lvl_name, translation=[0, self.level_height, 0])
-        cmds.xform(lvl_name, translation=[0, level_lift+(0.5*self.level_height), 0])
+        cmds.xform(lvl_name, pivots=[0, -level_height/2, 0])
+        cmds.xform(lvl_name, translation=[0, level_height, 0])
+        cmds.xform(lvl_name, translation=[0, level_lift+(0.5*level_height), 0])
         Cake._freeze_transforms(self, lvl_name)
         return lvl_name
 
@@ -33,12 +33,12 @@ class Cake():
                    level_lift+level_height, 0])
         return icing_name
 
-    def calculate_width(self, level, cake_width, level_proportion):
+    def calculate_width(self, level):
         # this is where we apply the level_proportion to the cake level.
         # we want the levels to get progressively smaller as they increase.
         # on level 0, we want the width to be unchanged.
         # level_width = cake_width
-        level_width = (level_proportion**level)*cake_width
+        level_width = (self.level_proportion**level)*self.cake_width
         return level_width
     
     def color_cake():
@@ -50,16 +50,18 @@ class Cake():
         cake_list = []
         level_height = self.cake_height/float(self.levels)
         for level in range(0, self.levels):
-            level_width = Cake.calculate_width()
-            lvl_name = Cake.generate_level(self, level_width, level_height)
+            level_width = Cake.calculate_width(self, level)
+            lvl_name = Cake.generate_level(self, level,
+                                           level_width, level_height)
             cake_list.append(lvl_name)
             if self.icing is True:
-                icing_name = Cake.generate_icing()
+                icing_name = Cake.generate_icing(self, level_width,
+                                                 level_height, level)
                 cake_list.append(icing_name)
         grp_name = cmds.group(cake_list, name='cake_GRP')
         cmds.xform(grp_name, pivots=[0, 0, 0])
-        if cake_color != "white":
-            Cake.color_cake(grp_name, cake_color)
+        if self.cake_color != "white":
+            Cake.color_cake(self, grp_name)
 
     def _freeze_transforms(self, obj_name):
         cmds.makeIdentity(obj_name, apply=True, translate=True,
@@ -79,3 +81,11 @@ class Cake():
 # in maya: create CakeWin object
 # win = project1.CakeWin()
 # win.show###
+
+# in maya script editor:
+# import project1
+# import importlib
+# importlib.reload(project1)
+
+# cake1 = project1.Cake()
+# cake1.generate_cake()
